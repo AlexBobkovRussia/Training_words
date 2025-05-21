3bfc3b314ace713a99a448139d9628fb10618ad5
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2025-05-21 22:45:31 +0300

    21.05.2025

--- backend/loading_words.py
+++ backend/loading_words.py
@@ -1,25 +1,42 @@
-import psycopg2
+import os
 
 
 class LoadWords:
-    def __init__(self):
-        self.__conn = psycopg2.connect(dbname="Words",
-                                       user="postgres",
-                                       password="asdfvcxz16022011",
-                                       host="95.79.11.225",
-                                       port=5433)
-        self._cursor = self.__connection.cursor()
+    def __init__(self, file_name: str):
+        if not os.path.exists(file_name):
+            with open(file_name, 'w', encoding='utf-8') as file:
+                pass
+        self._file_name = file_name
 
-    def load_words(self, words: dict[str, str]) -> None:
+    def load_words(self, words: dict[str, str | list[str]]) -> None:
         """_summary_
 
         Args:
             words (dict[str, str]): dictionary. First value must be an original word.
             Second value must be a translation.
         """
-        for original, translation in words.items():
-            pass
+        with open(self._file_name, 'r+', encoding='utf-8') as file:
+            for original, translations in words.items():
+                file.write(f"{original}: {translations}\n")
+
+    def get_words(self) -> dict[str, str | list[str]]:
+        with open(self._file_name, 'r+', encoding='utf-8') as file:
+            words = {}
+            for line in file.readlines():
+                line = line.strip()
+                line = line.split(": ")
+                exist = words.get(line[0])
+                if exist:
+                    words[line[0]].add(line[1])
+                else:
+                    words[line[0]] = set()
+                    words[line[0]].add(line[1])
+            for key, value in words.items():
+                words[key] = list(value)
+        return words
 
 
 if __name__ == '__main__':
-    loading = LoadWords()
+    loading = LoadWords("words.txt")
+    loading.load_words({"coffee": "кофе", "eat": "есть"})
+    words_for_train = loading.get_words()
---
+++ backend/train_words.py
@@ -0,0 +1,32 @@
+from random import shuffle
+
+from loading_words import LoadWords
+
+
+class TrainWords:
+    def __init__(self):
+        self._words = LoadWords("words.txt").get_words()
+
+    def train(self):
+        score = {}
+        words = self._words
+        for word in self._words.keys():
+            score[word] = 0
+
+        while not all(sc == 3 for sc in score.values()):
+            for word in self._words.keys():
+                if score[word] == 3:
+                    continue
+                result = yield word
+                if result in words[word]:
+                    score[word] += 1
+
+
+if __name__ == '__main__':
+    train = TrainWords().train()
+    next(train)
+    for _ in train:
+        train_word = next(train)
+        print(train_word)
+        word = input()
+        train.send(word)
